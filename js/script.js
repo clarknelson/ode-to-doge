071ebbe4340a50af7d6a719c2a4d8cb311dab44d
Clark Nelson <email@clarknelson.com>
2022-08-31 10:55:37 -0500

    fix gif sidebar

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
 /*          sorry for this code          */
 /* may the js gods have mercy on my soul */
 function changeBackground(){
-  var regexp = /(\d+)(?!.*\d)/;
-  var background_src = document.getElementById("left-gifs").style.background;
-  var background_count = parseInt(regexp.exec(background_src)[0]);
+  var leftGifs = document.getElementById("left-gifs");
+  var rightGifs = document.getElementById("right-gifs");
+
+  var regexp = /dogeSidebar(\d)/;
+  var background_src = leftGifs.style.background;
+  var background_count = parseInt(regexp.exec(background_src)[1]);
+  
+  // make sure we select the correct new bg
   var new_bg_cnt = background_count+1;
   if( background_count == 5 ){
     new_bg_cnt = 1;
   }
+
+  // replace the number of the background in the src
   var new_bg_src = background_src.replace(background_count, new_bg_cnt);
 
-  document.getElementById("left-gifs").style.background=new_bg_src;
-  document.getElementById("left-gifs").style.backgroundSize="100%";
-  document.getElementById("right-gifs").style.background=new_bg_src;
-  document.getElementById("right-gifs").style.backgroundSize="100%";
+  leftGifs.style.background=new_bg_src;
+  leftGifs.style.backgroundSize="100%";
+  rightGifs.style.background=new_bg_src;
+  rightGifs.style.backgroundSize="100%";
 }
